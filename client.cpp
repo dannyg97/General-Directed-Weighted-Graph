90cc964fecc7f4797e6dba88ac8e611b1479ed7b
Danny <38267274+dannyg97@users.noreply.github.com>
2019-08-02 22:56:00 +1000

    Specific test cases

--- client.cpp
+++ client.cpp
@@ -6,35 +6,32 @@
 // Note: At the moment, there is no client.sampleout. Please do your own testing
 
 int main() {
+  // Test basic iterator constructor works
   std::vector<std::string> v{"hello", "how", "are"};
-  gdwg::Graph<std::string, int> g{v.begin(), v.end()};
-  std::cout << g;
+  gdwg::Graph<std::string, int> a{v.begin(), v.end()};
+  std::cout << a << std::endl;
 
+  // Test tuple iterator constructor works
+  std::string s1{"Hello"};
+  std::string s2{"Bye"};
+  std::string s3{"Gday"};
+  auto e1 = std::make_tuple(s1, s2, 5.4);
+  auto e2 = std::make_tuple(s2, s3, 7.6);
+  auto e = std::vector<std::tuple<std::string, std::string, double>>{e1, e2};
+  gdwg::Graph<std::string, double> b{e.begin(), e.end()};
+  std::cout << b << std::endl;
 
-//  g.InsertNode("hello");
-//  g.InsertNode("how");
-//  g.InsertNode("are");
-//  g.InsertNode("you?");
-//
-//  g.InsertEdge("hello", "how", 5);
-//  g.InsertEdge("hello", "are", 8);
-//  g.InsertEdge("hello", "are", 2);
-//
-//  g.InsertEdge("how", "you?", 1);
-//  g.InsertEdge("how", "hello", 4);
-//
-//  g.InsertEdge("are", "you?", 3);
-//
-//  std::cout << g << '\n';
-//
-//  gdwg::Graph<std::string, int> g2{g};
-//
-//  std::cout << g2 << "\n";
-//
-//  // This is a structured binding.
-//  // https://en.cppreference.com/w/cpp/language/structured_binding
-//  // It allows you to unpack your tuple.
-//  for (const auto& [from, to, weight] : g) {
-//    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
-//  }
+  // Test initialiser list constructor works
+  gdwg::Graph<char, std::string> c{'a', 'b', 'x', 'y'};
+  std::cout << c << std::endl;
+
+  // Test insert node works
+  std::cout << "Inserting node" << std::endl;
+  c.insertNode('c');
+  std::cout << c << std::endl;
+
+  // Test insert edge works
+  std::cout << "Inserting edge" << std::endl;
+  c.insertEdge('a', 'b', "five");
+  std::cout << c << std::endl;
 }
